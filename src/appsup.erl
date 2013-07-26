bb6d331799ca2e14c289e6ed7fa0d1d5a8d20fba
maxvel <loz.accs@gmail.com>
2013-07-26 18:45:52 +0400

    Added watch_me api call.

--- src/appsup.erl
+++ src/appsup.erl
@@ -1,12 +1,17 @@
 -module(appsup).
 
--export([start/0, stop/0, watch/2, unwatch/1, show_apps/0]).
+-export([start/0    , stop/0, 
+         watch/2    , unwatch/1,
+         watch_me/1 , show_apps/0]).
 
 start() ->
   application:start(appsup).
 
 stop() ->
   application:stop(appsup).
+
+watch_me(Timeout) ->
+  restarter:watch(application:get_application(), Timeout).
 
 watch(App, Timeout) ->
   restarter:watch(App, Timeout).
